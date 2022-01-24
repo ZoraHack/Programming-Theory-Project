cf885f9c12abfe17cd9e2b8673fc7f85e3864c2c
ZoraHack <52333453+ZoraHack@users.noreply.github.com>
2022-01-23 21:27:33 -0500

    secondary commit
    new assets added

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    [SerializeField] Vector3 offset = new Vector3(-1, 9, -19);
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //Offset the camera behind the player by adding to the player's position
+
+        transform.position = player.transform.position + offset;
+
+    }
+}
